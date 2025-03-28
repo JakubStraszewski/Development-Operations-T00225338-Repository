8443479fd4fd31a7f9f97bcea4ff3099204d4253
JakubStraszewski <119119966+JakubStraszewski@users.noreply.github.com>
2025-03-28 11:38:58 +0000

    Introduce Error
    Introduce Error

--- DevOpsSampleApplicationClient.java
+++ DevOpsSampleApplicationClient.java
@@ -5,7 +5,7 @@ public class DevOpsSampleApplicationClient {
 	public static void main(String args[]) {
 		try {
 			Socket serverSocket = new Socket(InetAddress.getByName("localhost"), 8087);
-			OutputStream stream = serverSocket.getOutputStream();
+			OutputStream sream = serverSocket.getOutputStream();
 			stream.write("Test".getBytes());
 		} catch (IOException ex) {
 			ex.printStackTrace();
