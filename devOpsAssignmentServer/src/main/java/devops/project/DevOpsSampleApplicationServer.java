dc36b4d42d91750c7ad73be70d6f837fdf6d66bb
JakubStraszewski <119119966+JakubStraszewski@users.noreply.github.com>
2025-04-05 10:34:22 +0100

    Add files via upload

---
+++ devOpsAssignmentServer/src/main/java/devops/project/DevOpsSampleApplicationServer.java
@@ -0,0 +1,17 @@
+import java.io.*;
+import java.net.*;
+
+public class DevOpsSampleApplicationServer {
+	public static void main(String args[]) {
+		try {
+			ServerSocket serverSocket = new ServerSocket(8087);
+			Socket clientSocket = serverSocket.accept();
+			InputStream stream = clientSocket.getInputStream();
+			byte[] buffer = new byte[10];
+			stream.read(buffer, 0, 10);
+			System.out.println(new String(buffer));
+		} catch (IOException ex) {
+			ex.printStackTrace();
+		}
+	}
+}
---
+++ devOpsAssignmentServer/src/test/java/devops/project/DevOpsSampleApplicationServerTest.java
@@ -0,0 +1,19 @@
+package devops.project;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class DevOpsSampleApplicationServerTest {
+
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue() {
+        assertTrue(true);
+    }
+}
